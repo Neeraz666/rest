732454e0eaf75bac65c67899ebe3340c7251aa2e
Niraj Pandey <neeraz.pandey2058@gmail.com>
2023-08-27 11:50:38 +0545

    Get and Post request create.

--- rest/home/views.py
+++ rest/home/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
 # Create your views here.
 
-@api_view(['POST'])
+@api_view(['GET'])
 def home(request):
-    return Response({'status':200, 'message':'Hello World!'})
+    studentobj = Student.objects.all()
+    serializer = StudentSerializer(studentobj, many=True)
+    return Response({'status':200, 'message':serializer.data})
+
+@api_view(['POST'])
+def post_student(request):
+    data = request.data
+    print(data)
+    return Response({'status':200, 'payload':data, 'message': 'you sent'})
